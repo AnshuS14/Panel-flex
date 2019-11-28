d969812e6336cec2f7420df815363e06a6e19f1d
Anshu Saxena <anshu@so.city>
2019-11-28 16:07:45 +0530

    flex understanding

---
+++ panel.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+function toggleActive(e){
+    this.classList.toggle('open-active');
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
